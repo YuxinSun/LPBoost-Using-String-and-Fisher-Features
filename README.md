b094890c609398c92361c304ac30b2cd83bf5352
YuxinSun <yuxin.sun.13@ucl.ac.uk>
2016-07-10 00:27:32 +0100

    minor modifications

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # LPBoost-Using-String-and-Fisher-Features
 Linear programming boosting (LPBoost) with string and Fisher features
 
-This repository provides code and data sets that have been used for paper (link).
+This repository provides code and data sets that have been used.
 
 Features contains code to generate both string and Fisher features. LPBoost contains code to perform LPBoost.
 
--- sample_code.py
+++ sample_code.py
@@ -2,6 +2,7 @@ __author__ = 'yuxinsun'
 
 from scipy.io import loadmat, savemat
 import numpy as np
+from sklearn.metrics import accuracy_score
 from Features.process_data import save_pickle, read_pickle
 from LPBoost.lpboost import lpboost
 from Features.generate_features import feature_generation, create_word_list
@@ -10,18 +11,22 @@ from Features.generate_features import feature_generation, create_word_list
 #      'CASSLGGPDTQYFGPG CASSRTGRDEQYFGPG CARNVTPPKSYAVFFGKG CAWSLQGYNSPLYFGEG CASRQGDTNERLFFGHG CASSLDGGPEQYFGPG CASSDARGDQDTQYFGPG CASSPRDNYNSPLYFGEG']
 # save_pickle('', 'toy_data', a)
 
-print('Generate string features using toy_data.cpickle')
+print('This step shows how to generate feature matrix using CDR3 sequences.'
+      '\nGenerate string features using toy_data.cpickle')
 data = read_pickle('', 'toy_data')
 f = feature_generation(feature_type='fisher')
 data_norm = f.process(data)
 
-print('\nPerform LPBoost using hh1.mat')
-data = loadmat('hh1.mat', variable_names=['data_norm'])
+print('\nThis step shows how to perform LPBoost using a toy feature matrix containing 33 samples.'
+      '\nPerform LPBoost using toy_feature.mat')
+data = loadmat('toy_feature.mat', variable_names=['data_norm'])
 data = np.hstack((data['data_norm'], -data['data_norm']))
 label = np.hstack((-np.ones(15), np.ones(18)))
 lp = lpboost(nu=0.5, threshold=10**-3, verbose=1)
 
-a = lp.fit(data, label)  # fit LPBoost model
-b = lp.fit_transform(data, label)  # fit and transform
-c = lp.transform(data)  # transform data using fitted LPBoost model
-d = lp.predict(data)  # predict labels using LPBoost classifier
+# for convenience data is not split into training and test set
+lp.fit(data, label)  # fit LPBoost model
+label_pred = lp.predict(data)  # predict labels using LPBoost classifier
+acc = accuracy_score(label, label_pred)  # compute training accuracy
+
+print('\nTraining accuracy is %.2f' % acc)
